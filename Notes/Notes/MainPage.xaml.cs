8a900eb4e7b899a0ddf2c7f09e89002faeef781a
Guilherme Andrade <guilherme-andrade1110@hotmail.com>
2019-08-15 23:09:41 -0300

    Alocando o projeto

---
+++ Notes/Notes/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+            DisplayAlert("Anotado","Suas anotações foram salvas","OK");
+       
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+                DisplayAlert("Apagado", "Suas anotações foram apagadas", "OK");
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
