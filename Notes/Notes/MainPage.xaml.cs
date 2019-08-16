728f49953aabcda4aead33ab258143040e41ff06
Guilherme Andrade <guilherme-andrade1110@hotmail.com>
2019-08-16 09:29:08 -0300

    Add animation if editor is null

--- Notes/Notes/MainPage.xaml.cs
+++ Notes/Notes/MainPage.xaml.cs
@@ -6,12 +6,12 @@ namespace Notes
 {
     public partial class MainPage : ContentPage
     {
-        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        public string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
 
         public MainPage()
         {
             InitializeComponent();
-            //Animation();
+            Animation();
 
             if (File.Exists(_fileName))
             {
@@ -19,16 +19,24 @@ namespace Notes
             }
         }
 
-        void OnSaveButtonClicked(object sender, EventArgs e)
+        async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(editor.Text))
             {
-                DisplayAlert("Ops", "Nenhuma anotação foi feita", "OK");
+                uint timeout = 50;
+                await editor.TranslateTo(-15, 0, timeout);
+                await editor.TranslateTo(15, 0, timeout);
+                await editor.TranslateTo(-10, 0, timeout);
+                await editor.TranslateTo(10, 0, timeout);
+                await editor.TranslateTo(-5, 0, timeout);
+                await editor.TranslateTo(5, 0, timeout);
+                editor.TranslationX = 0;
+                await DisplayAlert("Ops", "Nenhuma anotação foi feita", "OK");
             }
             else
             {
                 File.WriteAllText(_fileName, editor.Text);
-                DisplayAlert("Anotado", "Suas anotações foram salvas", "OK");
+                await DisplayAlert("Anotado", "Suas anotações foram salvas", "OK");
             }
 
         }
